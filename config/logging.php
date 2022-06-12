0c84548fc3fb1bb47acc3fa41d98cbe666bd3535
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-06-12 22:41:35 +0700

    :zombie: Formatter

--- config/logging.php
+++ config/logging.php
@@ -103,7 +103,7 @@ return [
             'driver' => 'monolog',
             'level' => env('LOG_LEVEL', 'debug'),
             'handler' => StreamHandler::class,
-            'formatter' => env('LOG_STDERR_FORMATTER'),
+            'formatter' => \Monolog\Formatter\JsonFormatter::class,
             'with' => [
                 'stream' => storage_path("logs/application.log"),
             ],
